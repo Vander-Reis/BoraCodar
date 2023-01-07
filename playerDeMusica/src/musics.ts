04841cf3ed7d96b69ad9c55f14c8ba1414e6f29a
Vander-Reis <vanderreis2017@outlook.com>
2023-01-07 20:01:00 -0300

    first commit

---
+++ playerDeMusica/src/musics.ts
@@ -0,0 +1,13 @@
+const musics = [
+    {
+        name: "musica1",
+        src: "../music/musica1.mp3"
+    },
+
+    {
+        name: "musica2",
+        src: "../music/musica1.mp3"
+    }
+]
+
+export { musics }
